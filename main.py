a569689a012223598bca9d48234a82d900e9fccb
root <root@ikari162.fvds.ru>
2023-04-21 17:56:22 +0300

    add scheme file

--- main.py
+++ main.py
@@ -4,4 +4,61 @@ app = FastAPI()
 
 @app.get('/')
 def home():
-        return {"key":"Hello"}
+        return [
+  {
+    "id": 1,
+    "image": "1.webp",
+    "name": "T-shirt 1",
+    "price": 150,
+    "article": "0001",
+    "available": 1
+  },
+  {
+    "id": 2,
+    "image": "2.webp",
+    "name": "T-shirt 2",
+    "price": 210,
+    "article": "0002",
+    "available": 1
+  },
+  {
+    "id": 3,
+    "image": "3.webp",
+    "name": "T-shirt 3",
+    "price": 180,
+    "article": "0003",
+    "available": 1
+  },
+  {
+    "id": 4,
+    "image": "4.webp",
+    "name": "T-shirt 4",
+    "price": 4000,
+    "article": "0004",
+    "available": 1
+  },
+  {
+    "id": 5,
+    "image": "5.webp",
+    "name": "T-shirt 5",
+    "price": 350,
+    "article": "0005",
+    "available": 1
+  },
+  {
+    "id": 6,
+    "image": "6.webp",
+    "name": "T-shirt 6",
+    "price": 250,
+    "article": "0006",
+    "available": 1
+  },
+  {
+    "id": 7,
+    "image": "6.webp",
+    "name": "T-shirt 7",
+    "price": 2550,
+    "article": "0007",
+    "available": 1
+  }
+]
